73b63621cd9b8bba3cc53d0a680882a90f38c8a5
Kiran Anand <liteninkiran@hotmail.com>
2024-05-04 17:51:25 +0100

    1:18:00 - React Router Setup

--- src/components/JobListing.tsx
+++ src/components/JobListing.tsx
@@ -9,55 +9,55 @@ const JobListing = ({ job }: Props) => {
     }
     return (
         // Container
-        <div className="bg-white rounded-xl shadow-md relative">
+        <div className='bg-white rounded-xl shadow-md relative'>
 
             {/* Container */}
-            <div className="p-4">
+            <div className='p-4'>
 
                 {/* Title | Type */}
-                <div className="mb-6">
+                <div className='mb-6'>
 
                     {/* Title */}
-                    <div className="text-gray-600 my-2">
+                    <div className='text-gray-600 my-2'>
                         {job.type}
                     </div>
 
                     {/* Job Type */}
-                    <h3 className="text-xl font-bold">
+                    <h3 className='text-xl font-bold'>
                         {job.title}
                     </h3>
 
                 </div>
 
                 {/* Job Description */}
-                <div className="mb-5">
+                <div className='mb-5'>
                     {description}
                 </div>
 
                 {/* Toggle Read More/Less */}
-                <button onClick={() => setShowFullDescription((prev) => !prev)} className="text-indigo-500 mb-5 hover:text-indigo-600">
+                <button onClick={() => setShowFullDescription((prev) => !prev)} className='text-indigo-500 mb-5 hover:text-indigo-600'>
                     { showFullDescription ? 'Less' : 'More' }
                 </button>
 
                 {/* Salary */}
-                <h3 className="text-indigo-500 mb-2">
+                <h3 className='text-indigo-500 mb-2'>
                     {job.salary}
                 </h3>
 
                 {/* Horizontal Line */}
-                <div className="border border-gray-100 mb-5"></div>
+                <div className='border border-gray-100 mb-5'></div>
 
                 {/* Container */}
-                <div className="flex flex-col lg:flex-row justify-between mb-4">
+                <div className='flex flex-col lg:flex-row justify-between mb-4'>
 
                     {/* Location */}
-                    <div className="text-orange-700 mb-3">
+                    <div className='text-orange-700 mb-3'>
                         <FaMapMarker className='inline text-lg mb-1 mr-1' />
                         {job.location}
                     </div>
 
                     {/* Read More Link */}
-                    <a href={`/job/${job.id}`} className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm">
+                    <a href={`/job/${job.id}`} className='h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm'>
                         Read More
                     </a>
 
