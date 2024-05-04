86c94ea4edbf00be67252a34c0268a7fa7bfbe74
Kiran Anand <liteninkiran@hotmail.com>
2024-05-04 17:54:59 +0100

    1:21:36 - Router Provider

--- src/App.tsx
+++ src/App.tsx
@@ -5,25 +5,13 @@ import {
     RouterProvider,
 } from 'react-router-dom';
 
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import HomeCards from './components/HomeCards';
-import JobListings from './components/JobListings';
-import ViewAllJobs from './components/ViewAllJobs';
-
 const router = createBrowserRouter(
     createRoutesFromElements(<Route index element={<h1>My App</h1>} />)
 );
 
 const App = () => {
     return (
-        <>
-            <Navbar />
-            <Hero />
-            <HomeCards />
-            <JobListings />
-            <ViewAllJobs />
-        </>
+        <RouterProvider router={router} />
     );
 }
 
