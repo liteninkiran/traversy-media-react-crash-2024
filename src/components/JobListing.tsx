f40e23a28466e4499db351125e8d720a2aec0a40
Kiran Anand <liteninkiran@hotmail.com>
2024-05-04 18:08:35 +0100

    1:30:50 - Link Component

--- src/components/JobListing.tsx
+++ src/components/JobListing.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { FaMapMarker } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const JobListing = ({ job }: Props) => {
     const [showFullDescription, setShowFullDescription] = useState(false);
@@ -57,9 +58,9 @@ const JobListing = ({ job }: Props) => {
                     </div>
 
                     {/* Read More Link */}
-                    <a href={`/job/${job.id}`} className='h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm'>
+                    <Link to={`/job/${job.id}`} className='h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm'>
                         Read More
-                    </a>
+                    </Link>
 
                 </div>
             </div>
