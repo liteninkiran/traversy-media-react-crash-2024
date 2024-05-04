a5530a6ed09a50e7e6074feaf7b92cde6e0286e1
Kiran Anand <liteninkiran@hotmail.com>
2024-05-04 17:52:52 +0100

    1:20:21 - Create Routes From Elements

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import {
     Route, 
     createBrowserRouter, 
     createRoutesFromElements, 
-    RouterProvider
+    RouterProvider,
 } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
@@ -10,6 +10,10 @@ import Hero from './components/Hero';
 import HomeCards from './components/HomeCards';
 import JobListings from './components/JobListings';
 import ViewAllJobs from './components/ViewAllJobs';
+
+const router = createBrowserRouter(
+    createRoutesFromElements(<Route index element={<h1>My App</h1>} />)
+);
 
 const App = () => {
     return (
