ff38824be267bb94f40ef16a244e25f1a579da53
Kiran Anand <liteninkiran@hotmail.com>
2024-05-04 22:35:01 +0100

    2:58:54 - PUT Request to Update Job

--- src/App.tsx
+++ src/App.tsx
@@ -35,6 +35,15 @@ const App = () => {
     };
 
     const updateJob = async (job: Job) => {
+        const res = await fetch(`/api/jobs/${job.id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(job),
+        });
+        console.log(res);
+        return;
     };
 
     const router = createBrowserRouter(
