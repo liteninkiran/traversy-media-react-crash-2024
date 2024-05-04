ad495963881b0be024543151b6a9cf8abe03470d
Kiran Anand <liteninkiran@hotmail.com>
2024-05-04 17:29:01 +0100

    58:50 - Create Lists With map()

--- src/components/JobListings.tsx
+++ src/components/JobListings.tsx
@@ -1,7 +1,21 @@
 import jobs from '../jobs.json';
 
+interface Job {
+    id: number;
+    title: string;
+    type: string;
+    description: string;
+    location: string;
+    salary: string;
+    company: {
+        name: string;
+        description: string;
+        contactEmail: string;
+        contactPhone: string;
+    }
+}
+
 const JobListings = () => {
-    console.log(jobs);
     return (
         <section className="bg-blue-50 px-4 py-10">
             <div className="container-xl lg:container m-auto">
@@ -9,87 +23,63 @@ const JobListings = () => {
                     Browse Jobs
                 </h2>
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-                    {/* Job Listing 1 */}
-                    <div className="bg-white rounded-xl shadow-md relative">
-                        <div className="p-4">
-                            <div className="mb-6">
-                                <div className="text-gray-600 my-2">Full-Time</div>
-                                <h3 className="text-xl font-bold">Senior React Developer</h3>
-                            </div>
+                    {
+                        jobs.map((job) => {
+                            return (
+                                // Container
+                                <div className="bg-white rounded-xl shadow-md relative">
 
-                            <div className="mb-5">
-                                We are seeking a talented Front-End Developer to join our team in Boston, MA. The ideal candidate will have strong skills in HTML, CSS, and JavaScript...
-                            </div>
+                                    {/* Container */}
+                                    <div className="p-4">
 
-                            <h3 className="text-indigo-500 mb-2">$70 - $80K / Year</h3>
+                                        {/* Title | Type */}
+                                        <div className="mb-6">
 
-                            <div className="border border-gray-100 mb-5"></div>
+                                            {/* Title */}
+                                            <div className="text-gray-600 my-2">
+                                                {job.type}
+                                            </div>
 
-                            <div className="flex flex-col lg:flex-row justify-between mb-4">
-                                <div className="text-orange-700 mb-3">
-                                    <i className="fa-solid fa-location-dot text-lg"></i>
-                                    Boston, MA
+                                            {/* Job Type */}
+                                            <h3 className="text-xl font-bold">
+                                                {job.title}
+                                            </h3>
+
+                                        </div>
+            
+                                        {/* Job Description */}
+                                        <div className="mb-5">
+                                            {job.description}
+                                        </div>
+            
+                                        {/* Salary */}
+                                        <h3 className="text-indigo-500 mb-2">
+                                            {job.salary}
+                                        </h3>
+            
+                                        {/* Not sure what this is */}
+                                        <div className="border border-gray-100 mb-5"></div>
+            
+                                        {/* Container */}
+                                        <div className="flex flex-col lg:flex-row justify-between mb-4">
+
+                                            {/* Location */}
+                                            <div className="text-orange-700 mb-3">
+                                                <i className="fa-solid fa-location-dot text-lg"></i>
+                                                {job.location}
+                                            </div>
+
+                                            {/* Read More Link */}
+                                            <a href={`/job/${job.id}`} className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm">
+                                                Read More
+                                            </a>
+
+                                        </div>
+                                    </div>
                                 </div>
-                                <a href="job.html" className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm">
-                                    Read More
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                    {/* Job Listing 2 */}
-                    <div className="bg-white rounded-xl shadow-md relative">
-                        <div className="p-4">
-                            <div className="mb-6">
-                                <div className="text-gray-600 my-2">Remote</div>
-                                <h3 className="text-xl font-bold">Front-End Engineer (React)</h3>
-                            </div>
-
-                            <div className="mb-5">
-                                Join our team as a Front-End Developer in sunny Miami, FL. We are looking for a motivated individual with a passion...
-                            </div>
-
-                            <h3 className="text-indigo-500 mb-2">$70K - $80K / Year</h3>
-
-                            <div className="border border-gray-100 mb-5"></div>
-
-                            <div className="flex flex-col lg:flex-row justify-between mb-4">
-                                <div className="text-orange-700 mb-3">
-                                    <i className="fa-solid fa-location-dot text-lg"></i>
-                                    Miami, FL
-                                </div>
-                                <a href="job.html" className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm">
-                                    Read More
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                    {/* Job Listing 3 */}
-                    <div className="bg-white rounded-xl shadow-md relative">
-                        <div className="p-4">
-                            <div className="mb-6">
-                                <div className="text-gray-600 my-2">Remote</div>
-                                <h3 className="text-xl font-bold">React.js Developer</h3>
-                            </div>
-
-                            <div className="mb-5">
-                                Are you passionate about front-end development? Join our team in vibrant Brooklyn, NY, and work on exciting projects that make a difference...
-                            </div>
-
-                            <h3 className="text-indigo-500 mb-2">$70K - $80K / Year</h3>
-
-                            <div className="border border-gray-100 mb-5"></div>
-
-                            <div className="flex flex-col lg:flex-row justify-between mb-4">
-                                <div className="text-orange-700 mb-3">
-                                    <i className="fa-solid fa-location-dot text-lg"></i>
-                                    Brooklyn, NY
-                                </div>
-                                <a href="job.html" className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm">
-                                    Read More
-                                </a>
-                            </div>
-                        </div>
-                    </div>
+                            );
+                        })
+                    }
                 </div>
             </div>
         </section>
