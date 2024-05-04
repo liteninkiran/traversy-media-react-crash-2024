32e09c444e0ac2252463730ff5fde4dc8e7b18fa
Kiran Anand <liteninkiran@hotmail.com>
2024-05-04 16:53:14 +0100

    48:51 - Wrapper Components

---
+++ src/components/Card.tsx
@@ -0,0 +1,16 @@
+import { ReactNode } from 'react';
+
+interface Props {
+    children: ReactNode;
+    bg?: string;
+}
+
+const Card = ({children, bg = 'bg-gray-100'}: Props) => {
+    return (
+        <div className={`${bg} p-6 rounded-lg shadow-md`}>
+            {children}
+        </div>
+    );
+}
+
+export default Card;
